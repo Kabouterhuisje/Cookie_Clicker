6bbbc1e968ac26b81d48173fb4b473c42bfc5072
Gebruiker <gebruiker@gebruikers-mac.local>
2018-02-08 17:16:03 -0800

    Initial Commit

---
+++ Cookie Clicker/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  CookieClicker
+//
+//  Created by Koen van Brero on 30-01-18.
+//  Copyright © 2018 Paul Wagener. All rights reserved.
+//
+
+import Foundation
+
+enum Item : String {
+    case cookie = "🍪"
+    case grandma = "👵"
+    case robot = "🤖"
+    case farm = "🚜"
+    case mine = "⛏️"
+}
---
+++ Cookie Clicker/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Cookie Clicker
+//
+//  Created by Koen van Brero on 01-02-18.
+//  Copyright © 2018 Avans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var timesClicked = 0;
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func cookieClicked(_ sender: UIButton) {
+        timesClicked += 1
+        cookieAmount.text = "You clicked the cookie \(timesClicked) times"
+    }
+    @IBOutlet weak var cookieAmount: UILabel!
+    
+}
+
