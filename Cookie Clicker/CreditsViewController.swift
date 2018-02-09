7dece16222feb9085d5d06e443429fff2f6f34b4
Gebruiker <gebruiker@gebruikers-mac.local>
2018-02-08 17:25:45 -0800

    Added navigationController

--- Cookie Clicker/CreditsViewController.swift
+++ Cookie Clicker/CreditsViewController.swift
@@ -1,10 +1,4 @@
-//
-//  CreditsViewController.swift
-//  Cookie Clicker
-//
-//  Created by Gebruiker on 08/02/2018.
-//  Copyright © 2018 Avans. All rights reserved.
-//
+
 
 import UIKit
 
@@ -13,24 +7,12 @@ class CreditsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     @IBAction func backButtonHit(_ sender: Any) {
         self.dismiss(animated: true)
